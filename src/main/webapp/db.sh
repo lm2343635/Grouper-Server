4dba8f2aeba4cbd0fbe93cecf7e6a0ccb5942d08
Meng Li <lm2343635@126.com>
2017-07-26 21:00:35 +0900

    Update db.sh

--- src/main/webapp/db.sh
+++ src/main/webapp/db.sh
@@ -8,5 +8,5 @@ end=$4;
 for index in `seq $start $end`
 do
     echo "Creating database grouper${index}."
-	echo "drop database if exists grouper${index}; create database grouper${index} character set utf8 collate utf8_general_ci; use grouper${index}; source `pwd`/grouper.sql;" | mysql -u $username -p$password
+    echo "drop database if exists grouper${index}; create database grouper${index} character set utf8 collate utf8_general_ci; use grouper${index}; source `pwd`/grouper.sql;" | mysql -u $username -p$password
 done
